655c41ea5b5cb7f8d8d9d9eb395ed4b3dd25af0a
Rei Ishikawa <reimariodayo@gmail.com>
2018-05-08 12:21:57 +0900

    update 1.1.0

--- app/src/main/java/io/karte/tracker_sample/MyFirebaseMessagingService.java
+++ app/src/main/java/io/karte/tracker_sample/MyFirebaseMessagingService.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -15,33 +14,27 @@ import com.google.firebase.messaging.RemoteMessage;
 import java.util.Map;
 
 import io.karte.android.tracker.Tracker;
-import io.karte.android.tracker.firebase.KarteMessageHandler;
+import io.karte.android.tracker.firebase.MessageHandler;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
   @Override
   public void onMessageReceived(RemoteMessage remoteMessage) {
 
-    if (remoteMessage.getNotification() != null) {
-      RemoteMessage.Notification notification = remoteMessage.getNotification();
-      sendNotification(notification.getTitle(), notification.getBody(), remoteMessage.getData());
-      return;
-    }
-
-    if (remoteMessage.getData() != null) {
+    boolean handled = MessageHandler.handleMessage(this, remoteMessage);
+    if (!handled) {
       Map<String, String> data = remoteMessage.getData();
       String title = data.get("subject");
       String body = data.get("text");
-      sendNotification(title, body, data);
+      sendNotification(title, body, data.get("android_channel_id"), data);
     }
   }
 
-  protected void sendNotification(String title, String body, Map<String, String> data) {
+  protected void sendNotification(String title, String body,String channel, Map<String, String> data) {
 
     Intent intent = new Intent(this, MainActivity.class);
     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-    KarteMessageHandler.copyInfoToIntent(data, intent);
+    MessageHandler.copyInfoToIntent(data, intent);
 
     PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
             PendingIntent.FLAG_ONE_SHOT);
@@ -49,17 +42,21 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
     NotificationCompat.Builder notificationBuilder =
-            new NotificationCompat.Builder(this)
-                    .setSmallIcon(android.R.drawable.sym_def_app_icon)
+            new NotificationCompat.Builder(this, "my_channel")
+                    .setSmallIcon(getApplicationInfo().icon)
                     .setContentTitle(title)
                     .setContentText(body)
                     .setAutoCancel(true)
                     .setSound(defaultSoundUri)
+                    .setChannelId(channel)
                     .setContentIntent(pendingIntent);
 
     NotificationManager notificationManager =
             (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
+    if (notificationManager == null) {
+      return;
+    }
     notificationManager.notify(0, notificationBuilder.build());
   }
 }
--- app/src/main/java/io/karte/tracker_sample/SampleApp.java
+++ app/src/main/java/io/karte/tracker_sample/SampleApp.java
@@ -1,6 +1,11 @@
 package io.karte.tracker_sample;
 
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 
 import io.karte.android.tracker.Tracker;
 
@@ -12,5 +17,19 @@ public class SampleApp extends Application {
   public void onCreate() {
     super.onCreate();
     Tracker.init(this, APP_KEY);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      createChannel(this);
+    }
+  }
+
+  @RequiresApi(api = 26)
+  public static void createChannel(final Context context) {
+    NotificationChannel channel = new NotificationChannel("my_channel", "通知テストチャンネル", NotificationManager.IMPORTANCE_DEFAULT);
+    channel.setDescription("テストの説明です");
+    channel.setShowBadge(true);
+
+    // create or update the Notification channel
+    final NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+    notificationManager.createNotificationChannel(channel);
   }
 }
