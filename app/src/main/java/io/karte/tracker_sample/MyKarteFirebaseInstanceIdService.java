e7675eaa6212666638c482a970d4c4ce9c1d1070
Leo <ando.reo420@gmail.com>
2019-04-24 21:02:15 +0900

    #5 FCM トークンの送信処理をKarte SDK v1.5.3対応する

--- app/src/main/java/io/karte/tracker_sample/MyKarteFirebaseInstanceIdService.java
+++ app/src/main/java/io/karte/tracker_sample/MyKarteFirebaseInstanceIdService.java
@@ -1,11 +1,17 @@
 package io.karte.tracker_sample;
 
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
 import io.karte.android.tracker.Tracker;
-import io.karte.android.tracker.firebase.KarteFirebaseInstanceIdService;
 
-public class MyKarteFirebaseInstanceIdService extends KarteFirebaseInstanceIdService {
+public class MyKarteFirebaseInstanceIdService extends FirebaseInstanceIdService {
 
-  protected Tracker getTracker() {
-    return Tracker.getInstance(this, SampleApp.APP_KEY);
+  @Override
+  public void onTokenRefresh() {
+    super.onTokenRefresh();
+    final String token = FirebaseInstanceId.getInstance().getToken();
+    Tracker.getInstance(this, SampleApp.APP_KEY).trackFcmToken(token);
   }
+
 }
